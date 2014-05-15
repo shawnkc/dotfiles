9907e19e1921293562917f958d289ee5e9520ce0
Shawn Casey <shawn.casey@gmail.com>
2014-05-15 09:30:46 -0500

    sublime shortcut and /usr/local/bin path

--- misc_aliases.sh
+++ misc_aliases.sh
@@ -2,3 +2,5 @@ alias s=subl
 alias l='ls -l'
 alias la='ls -la'
 alias lart='ls -lart'
+
+export PATH=$PATH:/usr/local/bin
---
+++ sublime.sh
@@ -0,0 +1,14 @@
+
+SUBLIME_TEXT_2="/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+SUBLIME_TEXT_3="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+
+if [[ -f "$SUBLIME_TEXT_2" ]]; then
+	rm /usr/local/bin/subl
+	ln -s "$SUBLIME_TEXT_2" /usr/local/bin/subl
+fi
+
+
+if [[ -e "$SUBLIME_TEXT_3" ]]; then
+	rm /usr/local/bin/subl
+	ln -s "$SUBLIME_TEXT_3" /usr/local/bin/subl
+fi

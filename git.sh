6ebb475b500e6c08385321e03f6e2460e190ba04
Shawn Casey <shawn.casey@gmail.com>
2014-05-16 08:57:19 -0500

    git aliases

--- git.sh
+++ git.sh
@@ -4,4 +4,6 @@
 # ex: git config -f .gitmodules submodule.<name>.branch <branch>
 
 alias git_submodule_update="git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch; git pull;'"
-alias git_submodule_status="git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git status;'"
+alias git_submodule_status="git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; echo; pwd; git status;'"
+alias git_status="git status;git_submodule_status"
+alias git_commit_sub="git commit -am 'sub';git push"

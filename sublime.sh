a96ccafb2abd34de17d2d4cd0ee3c023a79d5f99
Shawn Casey <shawn.casey@gmail.com>
2014-06-04 12:22:10 -0500

    rvm/subl edits

--- sublime.sh
+++ sublime.sh
@@ -1,15 +1,16 @@
 SUBLIME_TEXT_2="/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
 SUBLIME_TEXT_3="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
 
+mkdir -p /usr/local/bin
+
 if [[ -f "$SUBLIME_TEXT_2" ]]; then
-	rm /usr/local/bin/subl
-	ln -s "$SUBLIME_TEXT_2" /usr/local/bin/subl
+       rm /usr/local/bin/subl
+       ln -s "$SUBLIME_TEXT_2" /usr/local/bin/subl
 fi
 
-
 if [[ -e "$SUBLIME_TEXT_3" ]]; then
-	rm /usr/local/bin/subl
-	ln -s "$SUBLIME_TEXT_3" /usr/local/bin/subl
+       rm /usr/local/bin/subl
+       ln -s "$SUBLIME_TEXT_3" /usr/local/bin/subl
 fi
 
 export GIT_EDITOR=subl

e15757a1111e071301d0fbdcd6444bf383c18300
Shawn Casey <shawn.casey@gmail.com>
2014-11-21 11:36:59 -0600

    not using ~ in path, removing android studio from default adt location

--- android.sh
+++ android.sh
@@ -1,3 +1,6 @@
-export PATH="$PATH:/Applications/Android Studio.app/sdk/platform-tools"
-export PATH="$PATH:/Applications/Android Studio.app/sdk/tools"
+# export PATH="$PATH:/Applications/Android Studio.app/sdk/platform-tools"
+# export PATH="$PATH:/Applications/Android Studio.app/sdk/tools"
 
+export PATH="$PATH:/Users/Shawn/Development/adt-bundle-mac-x86_64-20140702/sdk/platform-tools/adb"
+export PATH="$PATH:/Users/Shawn/Development/adt-bundle-mac-x86_64-20140702/sdk/platform-tools"
+export PATH="$PATH:/Users/Shawn/Development/adt-bundle-mac-x86_64-20140702/sdk/tools"
